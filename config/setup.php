269e8616966e70125cf10a4bf372c64ba8d3e6de
mmanhack <mmanhack@school21>
2020-07-29 21:08:01 +0300

    added cookie

--- config/setup.php
+++ config/setup.php
@@ -1,6 +1,22 @@
 <?php
+// protection against session fixation
+$cookie_name = "user";
+if(!isset($_COOKIE[$cookie_name]))
+	setcookie($cookie_name, null, time() + (86400 * 30), "/");
+session_start();
 define('DB_NAME', 'camagru.sqlite');
-$recreate = true;
+$recreate = false;
+//print_r($_SERVER);
+//print_r($_SESSION);
+//if (isset($_GET['LOGOUT']) ||
+//    $_SERVER['REMOTE_ADDR'] !== $_SESSION['PREV_REMOTEADDR'] ||
+//    $_SERVER['HTTP_USER_AGENT'] !== $_SESSION['PREV_USERAGENT'])
+//    session_destroy();
+//session_regenerate_id(); // Generate a new session identifier
+//$_SESSION['PREV_USERAGENT'] = $_SERVER['HTTP_USER_AGENT'];
+//$_SESSION['PREV_REMOTEADDR'] = $_SERVER['REMOTE_ADDR'];
+// end of protection
+
 
 if (file_exists(ROOT_PATH . 'db/' . DB_NAME))
 {
@@ -12,3 +28,4 @@ if (file_exists(ROOT_PATH . 'db/' . DB_NAME))
 }
 else
     include_once (ROOT_PATH . 'config/database.php');
+
