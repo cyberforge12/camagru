6b865e02b64a12415b152853ec7095c21eee0ba4
mmanhack <mmanhack@school21>
2020-08-09 12:43:38 +0300

    updated database.php

--- config/database.php
+++ config/database.php
@@ -7,6 +7,10 @@ $sql_create_db = "create database `camagru.sqlite`;";
 
 @mkdir(ROOT_PATH . 'db');
 
+$recreate = false;
+if (file_exists(ROOT_PATH . 'db/' . DB_NAME) && $recreate == true)
+    unlink(ROOT_PATH . 'db/' . DB_NAME);
+
 try {
     global $dbh;
     $dbh = new PDO($DB_DSN, $DB_USER, $DB_PASSWORD);
@@ -18,24 +22,30 @@ try {
         password binary(64),
         is_confirmed integer,
         notify integer)');
+    $dbh->exec('create table if not exists uploads(
+        date timestamp DEAFULT CURRENT_TIMESTAMP,
+        photo,
+        photo_owner, foreign key (photo_owner) REFERENCES users(login))');
     $dbh->exec('create table if not exists photos(
-        id integer primary key,
+        date timestamp DEAFULT CURRENT_TIMESTAMP,
+        photo,
         photo_owner, foreign key (photo_owner) REFERENCES users(login))');
     $dbh->exec('create table if not exists comments(
         id integer primary key,
         text,
         comment_user REFERENCES users(login),
-        comment_photo REFERENCES photos(id))');
+        comment_photo REFERENCES photos(date))');
     $dbh->exec('create table if not exists likes(
         id integer primary key,
         like_user REFERENCES users(login),
-        like_photo REFERENCES photos(id))');
+        like_photo REFERENCES photos(date))');
     $dbh->exec('create table if not exists sessions(
-        session_id,
+        session_id VARCHAR(32),
         date,
         session_user REFERENCES users(login))');
-//    $sess_id = $_SESSION[PHPSESSID];
-//    $dbh->exec('insert into sessions VALUES ($sess_id, 2, 3)');
+    $dbh->exec('create table if not exists email_conf(
+        email REFERENCES users(email),
+        id)');
 } catch (PDOException $e) {
     echo 'Подключение не удалось: ' . $e->getMessage(); //TODO: REMOVE
 }
